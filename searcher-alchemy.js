746280709bae3c66bccfb477422e91bcbdb2003d
Toghrul000 <togrul.garalov@gmail.com>
2024-03-19 20:13:32 +0100

    receipts cached

--- searcher-alchemy.js
+++ searcher-alchemy.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const fs = require('fs');
 
 const cacheFilePath = './transactionsCache.json';
+const receiptsCacheFilePath = './receiptsCache.json';
 
 // Helper function to read from cache
 function readFromCache() {
@@ -19,6 +20,25 @@ function writeToCache(data) {
   fs.writeFileSync(cacheFilePath, JSON.stringify(data));
 }
 
+// Helper function to read from receipts cache
+function readFromReceiptsCache(txHash) {
+  if (fs.existsSync(receiptsCacheFilePath)) {
+    const cacheData = fs.readFileSync(receiptsCacheFilePath);
+    const receiptsCache = JSON.parse(cacheData);
+    return receiptsCache[txHash] || null;
+  }
+  return null;
+}
+
+// Helper function to write to receipts cache
+function writeToReceiptsCache(txHash, receiptData) {
+  let receiptsCache = {};
+  if (fs.existsSync(receiptsCacheFilePath)) {
+    receiptsCache = JSON.parse(fs.readFileSync(receiptsCacheFilePath));
+  }
+  receiptsCache[txHash] = receiptData;
+  fs.writeFileSync(receiptsCacheFilePath, JSON.stringify(receiptsCache));
+}
 
 const config = {
   apiKey: "alchemy-replit",
@@ -29,18 +49,15 @@ const alchemy = new Alchemy(config);
 async function getTransactionsInRange() {
 
     const WTH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
-    const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+    // const WBTC = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
 
     const startBlock = "0x129155C";
     const endBlock = "0x1291560";
 
     const cachedTransactions = readFromCache();
     if (cachedTransactions && cachedTransactions.startBlock === startBlock && cachedTransactions.endBlock === endBlock) {
-        // /console.log(cachedTransactions);
-        
         return cachedTransactions.transactions;
     }
-
 
     let response = await alchemy.core.getAssetTransfers({
         fromBlock: startBlock,
@@ -56,28 +73,29 @@ async function getTransactionsInRange() {
     return transactions;
 }
 
-//TODO: cache this
 async function getSwapEvents(txHash){
-    const receipt = await alchemy.core.getTransactionReceipt(txHash);
-    const swapEvents = receipt.logs.filter((log) => {
-      // Match the event signature to the Swap event
-      return log.topics[0] === Utils.id('Swap(address,uint256,uint256,uint256,uint256,address)');
-    });
-    return swapEvents.length; 
+  let receipt = readFromReceiptsCache(txHash);
+  if (!receipt) {
+    receipt = await alchemy.core.getTransactionReceipt(txHash);
+    writeToReceiptsCache(txHash, receipt);
   }
+  const swapEvents = receipt.logs.filter((log) => {
+    // Match the event signature to the Swap event
+    return log.topics[0] === Utils.id('Swap(address,uint256,uint256,uint256,uint256,address)');
+  });
+  return swapEvents.length; 
+}
 
 async function main() {
     // Example usage
 
     const transfers = await getTransactionsInRange();
-    //console.log(transfers);
     let count = 0;
 
     for (let i = 0; i < transfers.length; i++) {
         const swapEventCount = await getSwapEvents(transfers[i].hash);
         if (swapEventCount >= 2) {
-            //console.log(transfers[i]);
-            //console.log(count);
+
             count++;
         }
     }
